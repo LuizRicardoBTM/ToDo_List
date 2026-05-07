09f67b9a63e7517d506dfc2544fb076914ea05ba
Luiz Ricardo <luizricardo.btm@gmail.com>
2026-05-07 12:00:51 -0300

    Localhost server configuration

--- src/app.ts
+++ src/app.ts
@@ -4,7 +4,6 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
-app.use('/task');
 
 app.listen(PORT, () =>{
     console.log(`Server running on http://localhost:${PORT}`);
---
+++ src/routes/task.router.ts
@@ -0,0 +1,3 @@
+const taskRouter = 'task';
+
+export { taskRouter };
