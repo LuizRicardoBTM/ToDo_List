1933a3185905f8f53f83ca6f45f208589c40a610
Luiz Ricardo <luizricardo.btm@gmail.com>
2026-05-12 10:31:03 -0300

    Database creation and configuration of typescript CRUD commands

--- src/entity/entity.interface.ts
+++ src/entity/entity.interface.ts
@@ -1,13 +1,13 @@
 export default interface BaseInterface {
     get id():string;
-    get done():boolean;
-    get createdAt():Date;
-    get dueDate():Date;
-    validate():void 
+     
 }
 
 export interface TaskInterface extends BaseInterface {
     get title(): string,
     get description(): string,
-    validateTask(): void,
+    get done():boolean,
+    get createdAt():Date,
+    get dueDate():Date,
+    validateTask(): void
 }
--- src/repository/repository.interface.ts
+++ src/repository/repository.interface.ts
@@ -1,9 +1,8 @@
 import type { TaskInterface } from "../entity/entity.interface.js";
-import type BaseInterface from "../entity/entity.interface.js";
 
 export default interface TaskRepository {
-    create(task: BaseInterface): Promise<void>;
-    update(task: BaseInterface): Promise<void>;
+    create(task: TaskInterface): Promise<void>;
+    update(task: TaskInterface): Promise<void>;
     delete(id: string): Promise<void>;
     findById(id: string): Promise<TaskInterface | null>;
     findAll(): Promise<TaskInterface[]>
