774b1061ad39dee9500166ae91090f2cd1c0bcee
Luiz Ricardo <luizricardo.btm@gmail.com>
2026-05-13 11:12:21 -0300

    CRUD functions finished and errors handled

--- src/repository/task.repository.ts
+++ src/repository/task.repository.ts
@@ -3,6 +3,7 @@ import { PrismaBetterSqlite3 } from "@prisma/adapter-better-sqlite3";
 import type { TaskInterface } from "../entity/entity.interface.js";
 import type TaskRepositoryInterface from "./repository.interface.js";
 import { TaskEntity } from "../entity/task.entity.js";
+import { throwDeprecation } from "node:process";
 
 const adapter = new PrismaBetterSqlite3({
     url: process.env.DATABASE_URL
@@ -13,58 +14,82 @@ const prisma = new PrismaClient({adapter});
 class TaskRepository implements TaskRepositoryInterface{
     
     async create(task: TaskInterface): Promise<void> {
-        await prisma.task.create({
-            data: {
-                id: task.id,
-                title: task.title,
-                description: task.description,
-                priority: task.priority,
-                createdAt: task.createdAt,
-                done: task.done,
-                dueDate: task.dueDate
-            }
-        })
+        try{
+            await prisma.task.create({
+                data: {
+                    id: task.id,
+                    title: task.title,
+                    description: task.description,
+                    priority: task.priority,
+                    createdAt: task.createdAt,
+                    done: task.done,
+                    dueDate: task.dueDate
+                }
+            })
+        }
+        catch{
+            throw new Error("Couldn't create the task")
+        }
     }
 
     async delete(id: string): Promise<void> {
-        await prisma.task.delete({
-            where: {id: id}
-        })
-        
+        try{
+            await prisma.task.delete({
+                where: {id: id}
+            })
+        }
+        catch{
+            throw new Error("Couldn't delete the task")
+        }
     }
 
     async update(task: TaskInterface): Promise<void> {
-        await prisma.task.update({
-            where: {id: task.id},
-            data: {
-                title: task.title,
-                description: task.description,
-                priority: task.priority,
-                createdAt: task.createdAt,
-                done: task.done,
-                dueDate: task.dueDate
-            }
-        })
-        
+        try{    
+            await prisma.task.update({
+                where: {id: task.id},
+                data: {
+                    title: task.title,
+                    description: task.description,
+                    priority: task.priority,
+                    createdAt: task.createdAt,
+                    done: task.done,
+                    dueDate: task.dueDate
+                }
+            })
+        }
+        catch{
+            throw new Error("Couldn't update the task")
+        }
     }
 
     async findById(id: string): Promise<TaskEntity | null> {
-        const query = await prisma.task.findUnique({
-            where: {id: id}
-        })
+        try{
+            const query = await prisma.task.findUnique({
+                where: {id: id}
+            })
 
-        if (!query) {
-            return null;
+            if (!query) {
+                return null;
+            }
+
+            const task = new TaskEntity(query)
+            
+            return task;
+            }
+        catch{
+            throw new Error("Couldn't connect to the database")
         }
-
-        const task = new TaskEntity(query)
-        return task;
     }
 
     async findAll(): Promise<TaskInterface[]> {
-        const query = await prisma.task.findMany({})
+        try{
+            const query = await prisma.task.findMany({})
 
-        const task = new TaskEntity(query)
-        return task;
+            const tasks = query.map(task => new TaskEntity(task))
+            return tasks;
+        }
+        catch{
+            throw new Error("Couldn't connect to the database")
+        }
     }
 }
